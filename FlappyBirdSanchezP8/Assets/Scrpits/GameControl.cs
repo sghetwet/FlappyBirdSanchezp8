6c9447650afe407357c20d1c0e3040ceac5a7a4d
Adam Sanchez <722945@student.dusd.net>
2023-12-07 15:27:39 -0800

    added all sprites, sprite changes, background, and colliders, jumping, restart and playing, and moving background
    finished around 7 of the ten steps on the project, working on number 6 or 7 that I still need to continue, after UI and for the moving background.

---
+++ FlappyBirdSanchezP8/Assets/Scrpits/GameControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+    public GameObject GameOverText;
+    public bool gameOver = false;
+    public float scrollspeed = -1.5f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void BirdDied()
+    {
+        GameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
