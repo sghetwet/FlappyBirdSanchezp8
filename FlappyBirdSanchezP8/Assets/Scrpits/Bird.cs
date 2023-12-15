47664324176abe280c22aa8de6c9d7c0a527b705
Adam Sanchez <722945@student.dusd.net>
2023-12-15 14:41:55 -0800

    removed audio
    it aint finna work so ye the game is good but just without the audio

--- FlappyBirdSanchezP8/Assets/Scrpits/Bird.cs
+++ FlappyBirdSanchezP8/Assets/Scrpits/Bird.cs
@@ -9,15 +9,15 @@ public class Bird : MonoBehaviour
     private Rigidbody2D rb2d;
     public float upForce = 100;
     private Animator anim;
-    public AudioClip Score;
-    public AudioClip flap;
-    public AudioClip die;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator> ();
-        audioSource = GetComponent<AudioSource>(); 
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -30,7 +30,6 @@ public class Bird : MonoBehaviour
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
                 anim.SetTrigger("Flap");
-                PlaySound(flap);
             }
         }
     }
@@ -40,10 +39,5 @@ public class Bird : MonoBehaviour
         isDead = true;
         anim.SetTrigger("Die");
         GameControl.instance.BirdDied();
-        PlaySound(die);
-    }
-    public void PlaySound(AudioClip clip)
-    {
-        audioSource.PlayOneShot(clip);
     }
 }
